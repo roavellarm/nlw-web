789f22fd2ae3cb3614bb4fe5af2ef642d6e7969c
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-05 19:37:46 -0300

    Third day

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import backIcon from "../../assets/images/icons/back.svg";
 import logoImg from "../../assets/images/logo.svg";
-import './styles.css'
+import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+  const { title, description, children } = props;
+
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -20,6 +23,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
         {children}
       </div>
     </header>
